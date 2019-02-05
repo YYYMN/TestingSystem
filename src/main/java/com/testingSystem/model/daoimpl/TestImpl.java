a083dc29520dac757f9bbf8177d00608800f89aa
Владислав <https://github.com/VladislavNovikov/DeadValley>
2019-02-05 22:44:01 +0300

    3

--- src/main/java/com/testingSystem/model/daoimpl/TestImpl.java
+++ src/main/java/com/testingSystem/model/daoimpl/TestImpl.java
@@ -1,4 +1,20 @@
 package com.testingSystem.model.daoimpl;
 
-public class TestImpl {
+import com.sun.tools.javac.util.List;
+import com.testingSystem.model.dao.TestDao;
+import com.testingSystem.model.entity.Test;
+
+public class TestImpl implements TestDao {
+
+    public List<Test> getAllTests() {
+        return null;
+    }
+
+    public void addTest(Test test) {
+
+    }
+
+    public Test getTestById(Integer id) {
+        return null;
+    }
 }
